0d482223b43eaec35d55a8ff0f2e08047d658a94
gnomearcy <tomethelock@gmail.com>
2017-12-20 15:25:15 +0100

    Implementations of paging/filter contracts

---
+++ Project.Models/Filter/FilteringPayload.cs
@@ -0,0 +1,15 @@
+﻿using Project.Models.Common.Filtering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models.Filter
+{
+    public class FilteringPayload : IMakeFilterParameters
+    {
+        public string Name { get; set; }
+        public string Abrv { get; set; }
+    }
+}
---
+++ Project.Models/Paging/PagingPayload.cs
@@ -0,0 +1,17 @@
+﻿using Project.Models.Common.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models.Paging
+{
+    public class PagingPayload : IMakePageParameters
+    {
+        public int TargetPage { get; set; }
+        public int PageSize { get; set; }
+        public bool SortAsc { get; set; }
+        public SortType SortField { get; set; }
+    }
+}
